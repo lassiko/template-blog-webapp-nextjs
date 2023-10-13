08a450a8c5388d7263e6f1bbedc0b7e52a82cc53
Lassi K <lassi@koikkalainen.net>
2023-10-13 11:46:48 +0300

    next path

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
       {
         name: "lassicf1",
-        script: "./node_modules/.bin/next",
+        script: "./node_modules/next/dist/bin/next",
         args: "start -p " + (process.env.PORT || 3000),
         watch: false,
         autorestart: true,
